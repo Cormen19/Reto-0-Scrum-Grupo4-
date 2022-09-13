dfc88b9385734a5a7d6b9028a5c3642945897c77
georgenicusor.popa <georgenicusor.popa@elorrieta-errekamari.com>
2022-09-13 12:19:56 +0300

    Update reto0-scrum-grupo4.sql

--- Reto-0-Scrum-Grupo-4-2022-2023-main/Base de datos/reto0-scrum-grupo4.sql
+++ Reto-0-Scrum-Grupo-4-2022-2023-main/Base de datos/reto0-scrum-grupo4.sql
@@ -1,21 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 12-09-2022 a las 12:35:24
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `reto0-scrum-grupo4`
@@ -29,15 +17,37 @@ USE `reto0-scrum-grupo4`;
 -- Estructura de tabla para la tabla `usuario`
 --
 
-CREATE TABLE `usuario` (
-  `Idusuario` int(3) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `Nombre` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `Apellido` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `Rol` int(1) NOT NULL,
-  `Email` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
-  `Contrasena` varchar(50) COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+CREATE TABLE Usuario (
+  Id_Usuario int PRIMARY KEY AUTO_INCREMENT,
+  Nombre varchar(50)  NOT NULL,
+  Apellido varchar(50)  NOT NULL,
+  rol Enum("Profesor","Bedel","Seguridad","Alumno")NOT NULL,
+  Email varchar(200)  NOT NULL,
+  Contrasena varchar(50) NOT NULL
+) ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE Alarma_de_Incendios(
+Id_Alarma int primary key auto_increment,
+Encendido boolean not null default false
+);
+
+CREATE TABLE Calefacion(
+Id_Calefacion int primary key auto_increment,
+Encendido boolean not null default false
+);
+
+CREATE TABLE Aula(
+Id_aulacalefacionAula int  primary key auto_increment ,
+Alarma_de_Incendios int,
+Calefacion int,
+constraint fk_Alarma_de_Incendios foreign key (Alarma_de_Incendios) references Alarma_de_Incendios(Id_Alarma),
+constraint fk_Calefacion foreign key (Calefacion) references Calefacion(Id_Calefacion)
+);
+
+CREATE TABLE Monitoriza(
+Usuario int,
+Aula int, 
+constraint pk_Usuario_Aula primary key (Usuario,Aula),
+constraint fk_Usuario foreign key (Usuario) references Usuario(Id_Usuario),
+constraint fk_Aula foreign key (Aula) references Aula(Id_Aula)
+);
