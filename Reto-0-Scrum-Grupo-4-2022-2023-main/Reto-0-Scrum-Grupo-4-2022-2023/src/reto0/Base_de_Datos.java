0d630bc125d4df1f3ee71db853855e97edc65168
georgenicusor.popa <georgenicusor.popa@elorrieta-errekamari.com>
2022-09-20 12:22:56 +0300

    Update Base_de_Datos.java

--- Reto-0-Scrum-Grupo-4-2022-2023-main/Reto-0-Scrum-Grupo-4-2022-2023/src/reto0/Base_de_Datos.java
+++ Reto-0-Scrum-Grupo-4-2022-2023-main/Reto-0-Scrum-Grupo-4-2022-2023/src/reto0/Base_de_Datos.java
@@ -9,37 +9,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 
-
+/*La funcion de esta clase sirve para realizar todas las consultas necesarias a la base de datos que va desde consultas para obtener los datos hasta 
+ * modificaciones de el estado de las alarmas y calefaciones*/
 public class Base_de_Datos {
 	static Scanner teclado = new Scanner(System.in);
 	final static String url = "jdbc:mysql://localhost:33060/reto0-scrum-grupo4";
 	final static String Contra= "1234";
-	
-//	public static   ArrayList<Aula> aulas() throws ClassNotFoundException {
-//		 ArrayList<Aula> aula = new ArrayList<Aula>();
-//		 
-//		try {
-//			Class.forName("com.mysql.cj.jdbc.Driver");
-//			Connection conn = DriverManager.getConnection(url,"root",Contra);
-//		
-//			Statement s = conn.createStatement();
-//			ResultSet rs = s.executeQuery("Select * "
-//									+ "From aula;");
-//			System.out.println(rs);
-//
-//			while(rs.next()) {	
-//				Aula aux = new Aula();
-//							}	
-//			s.close();
-//			rs.close();
-//			conn.close();
-//		
-//		}catch(SQLException sql) {
-//			System.out.println("Error al conectar a la base de datos ");
-//		}
-//		return null;
-//		
-//	}
+	/*Recoge los datos de las calefacciones y las carga en memoria en un arraylist*/
 	public static ArrayList<Calefaccion> cargarCalefacion(){
 		 ArrayList<Calefaccion> calefaccion = new ArrayList<Calefaccion>();
 		 
@@ -70,7 +46,7 @@ public class Base_de_Datos {
 		return calefaccion;
 		
 	}
-	
+	/*Recoge los datos de las  alarmas y las carga en memoria en un arraylist*/
 	public static ArrayList<AlarmadeIncendios> cargarAlarmasdeIncendios(){
 		 ArrayList<AlarmadeIncendios> alarmadeIncendios = new ArrayList<AlarmadeIncendios>();
 		 
@@ -101,7 +77,8 @@ public class Base_de_Datos {
 		return alarmadeIncendios;
 		
 	}
-
+	/*Esta funcion recibe una insert por parametro despues de haber validado y comprobado que el usuario no existe
+	 * Y se guarda en la base de datos*/
 	public static boolean registrarUsuario(String datos) {
 		
 			try {
@@ -126,7 +103,10 @@ public class Base_de_Datos {
 			}
 
 	}
-
+	/*Esta funcion sirve para el login 
+	 * Verifica que el usuario este y coincida con la contraseña 
+	 * Si coinciden devulve true
+	 * Sino devuelve false*/
 	public static boolean usuarioValido(String nombre, String contrasenya) {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -155,7 +135,9 @@ public class Base_de_Datos {
 		
 		return false;
 	}
-
+	/*Verifica si en la base de datos hay algun usuario que tenga el mismo nombre y apellido que el que el de la persona que se intenta registrar si es asi
+	 *devuelve true 
+	 *Sino devuelve false*/
 	public static boolean UsuarioExiste(String nombre, String apellido) {
 		
 		try {
@@ -188,7 +170,7 @@ public class Base_de_Datos {
 	}
 	
 	
-	
+	/*Busca en la base de datos el ultimo usuario registro y devuelve ese numero +1 para pa creacion de otro usuario*/
 	public static int consultar_Ultimo_Usuario(){
 		int numero=0;
 		try {
@@ -214,6 +196,9 @@ public class Base_de_Datos {
 		return numero;
 	}
 
+	/*Esta funcion sirve para  cambiar el estado de uno de los elementos de la base de datos
+	 * Recibe un string que es el nombre del objeto al que se le va a cambiar el estado , el numero de registro que sea y true o false
+	 * Y se guarda el cambio en la base de datos*/
 	public static boolean CambioEstado(String string ,int indice, boolean b) {
 		String query= null;
 		try {
@@ -244,7 +229,9 @@ public class Base_de_Datos {
 		return false;
 	}
 	
-
+	/*Esta funcion sirve para  cambiar el estado de todos  los elementos de un tipo en la base de datos
+	 * Recibe un string que es el nombre del objeto al que se le va a cambiar el estado y true o false
+	 * Y se guarda el cambio en la base de datos*/
 	public static void EncenderoApagarTodo(String string,boolean b) {
 		String query = null;
 		try {
